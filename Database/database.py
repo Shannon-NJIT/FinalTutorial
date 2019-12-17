a8b53cd62f76a6af44a5c3986b8732e31001c94f
Shannon <sp358@njit.edu>
2019-12-17 18:07:42 -0500

    Feature:create code for database file to run

---
+++ Database/database.py
@@ -0,0 +1,38 @@
+from .models import db
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
+    Column, DateTime, ForeignKey, Numeric, SmallInteger
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker, Session
+
+engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
+
+model = declarative_base()
+
+def get_all(model):
+    data = model.query.all()
+    return data
+
+
+def add_instance(model, **kwargs):
+    instance = model(**kwargs)
+    db.session.add(instance)
+    commit_changes()
+
+
+def delete_instance(model, id):
+    model.query.filter_by(id=id).delete()
+    commit_changes()
+
+
+def edit_instance(model, id, **kwargs):
+    instance = model.query.filter_by(id=id).all()[0]
+    for attr, new_value in kwargs:
+        setattr(instance, attr, new_value)
+    commit_changes()
+
+
+def commit_changes():
+    db.session.commit()
+
+
+model.metadata.create_all(engine)
