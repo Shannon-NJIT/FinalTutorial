ce357f36ad6df1506235e32945084067c7e24ec3
Shannon <sp358@njit.edu>
2019-12-17 21:33:55 -0500

    Fix: updated updated __init__file to include UserModel and BlogpostModel

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,5 +3,7 @@ from flask_bcrypt import Bcrypt
 
 # initialize our db
 db = SQLAlchemy()
-
 bcrypt = Bcrypt()
+
+from .BlogpostModel import BlogpostModel, BlogpostSchema
+from .UserModel import UserModel, UserSchema
